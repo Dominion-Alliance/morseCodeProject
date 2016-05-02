b73de954a465db015b5d0c6404c2039c53a0cd77
CKertz <kertzc@live.com>
2016-05-02 08:38:54 -0500

    Added fixed .txt file, attempt at building tree. Still needs fixed

--- morseCodeProject/morseCodeProject/Source.cpp
+++ morseCodeProject/morseCodeProject/Source.cpp
@@ -1,8 +1,31 @@
+#include "Binary_Search_Tree.h"
+#include <fstream>
 #include <iostream>
+
 
 using namespace std;
 
+void displayTree(vector<string> a)
+{
+	
+	string textOutput;
+	ifstream inf;
+	inf.open("morseTree.txt");
+	while (!inf.eof())
+	{
+		getline(inf, textOutput);
+		a.push_back(textOutput);
+		// cout << textOutput << endl; 
+	}
+}
+
 int main()
 {
+	vector<string>buildTree;
+	displayTree(buildTree);
+	Binary_Search_Tree<string>morse;
+	morse.read_binary_tree(buildTree);
+
+	return 0;
 
 }
